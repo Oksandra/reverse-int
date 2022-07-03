57a692d99c1abf387876182df5bea410bc18dba9
Oksana <5oks5@mail.com>
2022-07-03 23:31:38 +0400

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    const positiveNumber = Math.abs(n);
+    const doToString = String(positiveNumber);
+    const amount = doToString.length;
+    let result = '';
+    let i = amount - 1;
+    while (i >= 0) {
+        result = `${result}${doToString[i]}`;
+        i = i - 1;
+    }
+    return +result;  
 }
